53df807552ec6446b1ddba5e9dfc9525bf834a7e
jonathonlaylo <jonathon.laylo@gmail.com>
2017-03-04 19:57:07 -1000

    working counter on server.js

--- routes/slow.js
+++ routes/slow.js
@@ -9,21 +9,6 @@ const redis = require('redis');
 
 const client = redis.createClient();
 
-client.on('error', function (err) {
-  console.log('error ' + err);
-});
-
-client.on('connect', sanityCheck);
-
-function sanityCheck(){
-  client.set('string key', 'hello world', function(err, reply){
-    console.log(reply.toString());
-  });
-  client.get('string key', function(err, reply){
-    console.log(reply.toString());
-  });
-}
-
 router.route('/')
   .get((req, res, next) => {
     return sleep(5000)
--- server.js
+++ server.js
@@ -2,21 +2,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
-
 const { slow } = require('./routes');
-
 const app = express();
 const PORT = 8080;
 
+const counter = 0;
+const redis = require('redis');
+const client = redis.createClient();
+
 app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
-
 app.use(bodyParser.json());
 // server.use(creamCache.init()); /* student implements this */
 app.use('/slow', slow);
 
+client.set('hello', 'world');
+
+client.get('hello', (error, value)=>{
+  console.log(value);
+});
+
 app.get('/', (req, res) => {
-  res.render('index');
+  client.incr('counter');
+  client.get('counter', (error, value)=>{
+    console.log(value);
+    res.render('index', {
+      'counter': value
+    });
+  });
 });
 
 app.listen(PORT, () => {
